bccc586a1e137e32f9761c1f9e42be842eac4945
Desckars <enzbrun8@gmail.com>
2025-09-23 18:33:21 -0300

    Cambios

---
+++ mcp_o3/src/test/java/o3/utec/mcp_o3/ProyectoIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package o3.utec.mcp_o3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
